6b5a6003262ba32b4199a8e4e3c40d4115569da8
Rodolphe Pineau <pineau@rti-zone.org>
2019-02-14 16:53:28 -0800

    Fix some small issue with calibration when the initial step per rev is very low.

--- x2dome.h
+++ x2dome.h
@@ -29,7 +29,7 @@ class MutexInterface;
 class BasicIniUtilInterface;
 class TickCountInterface;
 
-#define DRIVER_VERSION      2.1
+#define DRIVER_VERSION      2.11
 
 #define PARENT_KEY			"NexDome"
 #define CHILD_KEY_PORTNAME	"PortName"
