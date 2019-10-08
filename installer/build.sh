4d0ae08f880488544ec4351ca35162db43e2eaa0
Rodolphe Pineau <pineau@rti-zone.org>
2019-10-08 10:59:02 -0700

    macOS : Switch to 64 bit only, add script to notarize the macOS pkg

--- installer/build.sh
+++ installer/build.sh
@@ -6,12 +6,14 @@ cp "../NexDome.png" ROOT/tmp/NexDome_X2/
 cp "../domelist NexDome.txt" ROOT/tmp/NexDome_X2/
 cp "../build/Release/libNexDome.dylib" ROOT/tmp/NexDome_X2/
 
+PACKAGE_NAME="NexDome_X2.pkg"
+BUNDLE_NAME="org.rti-zone.NexDomeX2"
 if [ ! -z "$installer_signature" ]; then
-# signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.NexDome_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 NexDome_X2.pkg
-pkgutil --check-signature ./NexDome_X2.pkg
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
 else
-pkgbuild --root ROOT --identifier org.rti-zone.NexDome_X2 --scripts Scripts --version 1.0 NexDome_X2.pkg
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
 fi
 
 rm -rf ROOT
